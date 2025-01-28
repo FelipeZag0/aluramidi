7a071ebee1460bacb2d537474bbba1b62855eb2e
Felipe Zago <felipezago29@gmail.com>
2025-01-27 23:56:46 -0300

    projeto concluído

--- main.js
+++ main.js
@@ -1,5 +1,12 @@
-function tocaSom(idElementoAudio) {
-    document.querySelector(idElementoAudio).play();
+function tocaSom(seletorAudio) {
+    const elemento = document.querySelector(seletorAudio);
+
+    if (elemento && elemento.localName === 'audio') {
+        elemento.play();
+    }
+    else {
+        console.log('Elemento não encontrado ou seletor invalido');
+    }
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
@@ -8,7 +15,7 @@ const listaDeTeclas = document.querySelectorAll('.tecla');
 for (let contador = 0; contador < listaDeTeclas.length; contador++) {
     const tecla = listaDeTeclas[contador];
     const instrumento = tecla.classList[1];
-    
+
     // template string
     const idAudio = `#som_${instrumento}`;
 
@@ -16,4 +23,20 @@ for (let contador = 0; contador < listaDeTeclas.length; contador++) {
         tocaSom(idAudio);
     }
 
+    tecla.onclick = function () {
+        tocaSom(idAudio);
+    }
+
+    tecla.onkeydown = function (evento) {
+
+        console.log(evento.code);
+
+        if (evento.code === 'Space' || evento.code === 'Enter') {
+            tecla.classList.add('ativa');
+        }
+    }
+
+    tecla.onkeyup = function () {
+        tecla.classList.remove('ativa');
+    }
 }
